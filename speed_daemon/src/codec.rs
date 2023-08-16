7616c05e31f1504177335a2ac2d7586b688e22d9
shuai_yang <shuai_yang@intsig.net>
2023-08-16 15:24:07 +0800

    fix

--- speed_daemon/src/codec.rs
+++ speed_daemon/src/codec.rs
@@ -52,6 +52,7 @@ impl Decoder for ClientToServerCodec {
             }
             // want heartbeat message
             0x40 => {
+                println!("want heartbeat message: {:?}", src.clone());
                 let interval = BigEndian::read_u32(&src[1..5]);
                 return Ok(Some(ClientToServerMessage::WantHeartbeat { interval }));
             }
